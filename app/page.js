2f717f32ee99d98d345108f4be005dffa4708f57
kiran <kiran.s.dhawan@gmail.com>
2024-05-10 15:38:19 +0530

    1st commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+// import { Button } from "@/components/ui/button";
+import Hero from "./_components/Hero";
+export default function Home() {
+  return (
+    <div>
+      <Hero/>
+    </div>
+  );
+}
