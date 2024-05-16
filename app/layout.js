9d122374c58a790e558b65a07cded01292811294
kiran <kiran.s.dhawan@gmail.com>
2024-05-16 20:59:27 +0530

    Footer Added

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "./_components/Header";
 import NextAuthSessionProvider from "./provider";
 import { Toaster } from "@/components/ui/sonner";
+import Footer from "./_components/Footer";
 
 const inter = Outfit({ subsets: ["latin"] });
 
@@ -15,14 +16,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <NextAuthSessionProvider>
-        <div className=" mx-6 md:mx-16">
-          <Header/>
-          <Toaster />
-        {children}
-        </div>
-       </NextAuthSessionProvider>
-        </body>
+        <NextAuthSessionProvider>
+          <div className=" mx-6 md:mx-16">
+            <Header />
+            <Toaster />
+            {children}
+          </div>
+        </NextAuthSessionProvider>
+        <Footer />
+      </body>
     </html>
   );
 }
