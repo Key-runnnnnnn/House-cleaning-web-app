5c26f85a2a7e7da96c4bba61d69cb828b2a623f6
kiran <kiran.s.dhawan@gmail.com>
2025-07-07 16:24:46 +0530

    Some minor fix

--- app/_components/BusinessList.jsx
+++ app/_components/BusinessList.jsx
@@ -39,7 +39,7 @@ function BusinessList({businessList,title}) {
             ))
         :
         [1,2,3,4,5,6,7,8].map((item,index)=>(
-            <div className='w-full h-[300px]
+            <div key={index} className='w-full h-[300px]
             bg-slate-200 rounded-lg animate-pulse'>
             </div>
         ))
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,21 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['ap-south-1.graphassets.com', 'lh3.googleusercontent.com']
+    // domains: ['ap-south-1.graphassets.com', 'lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ap-south-1.graphassets.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   }
 }
 
